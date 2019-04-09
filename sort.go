95184f75b6ef1e1218258e182f1493e3d615745c
chenyayun <chenyayun@didichuxing.com>
2019-04-09 20:24:49 +0800

    commit

--- sort.go
+++ sort.go
@@ -1,12 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"sort"
+	"strings"
+	"fmt"
 )
 func main() {
-	checkList := []int64{4, 1, 3, 2}
-	sort.Slice(checkList, func(i, j int) bool {return checkList[i] >= checkList[j]})
+	sortSlice()
+	sortTest()
+}
 
-	fmt.Println(checkList)
+func sortSlice() {
+
+	var checkList []int64
+	checkList = append(checkList, 100)
+	checkList = append(checkList, 98)
+	checkList = append(checkList, 200)
+	checkList = append(checkList, 1)
+	sort.Slice(checkList, func(i, j int) bool {return checkList[i] <= checkList[j]})
+
+	strCheckList := strings.Replace(strings.Trim(fmt.Sprint(checkList), "[]"), " ", ",", -1)
+	fmt.Println(strCheckList)
+}
+
+func sortTest() {
+	panic(2)
+	data := make(map[string]string)
+	if data == nil {
+		fmt.Println("map nil")
+	} else {
+		fmt.Println("map not nil")
+	}
+
+	var checkNoList []int64
+	checkNoList = append(checkNoList, 677);
+	sort.Slice(checkNoList, func(i, j int) bool {
+		return checkNoList[i] <= checkNoList[j]
+	})
+	data["check_no_list"] = strings.Replace(strings.Trim(fmt.Sprint(checkNoList), "[]"), " ", ",", -1)
+
+	var hitNoList []int64
+	sort.Slice(hitNoList, func(i, j int) bool {
+		return hitNoList[i] <= hitNoList[j]
+	})
+	data["hit_no_list"] = strings.Replace(strings.Trim(fmt.Sprint(hitNoList), "[]"), " ", ",", -1)
+
+	fmt.Println(data)
 }
