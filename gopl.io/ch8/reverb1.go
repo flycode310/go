73d29d3ed7ad946a7008023e6fec958b6960fc17
chenyayun <cyy310_git>
2019-05-12 23:34:38 +0800

    reverb1

---
+++ gopl.io/ch8/reverb1.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net"
+	"time"
+	"fmt"
+	"strings"
+	"bufio"
+	"log"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+	time.Sleep(delay)
+}
+
+func HandleConnForReverb1(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo (c, input.Text(), 1 * time.Second)
+	}
+	c.Close()
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		HandleConnForReverb1(conn)
+	}
+
+}
